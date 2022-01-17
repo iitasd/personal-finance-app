660f9d4dab3aab143c93d2184c80cbcbd61c4b0e
isura <isuradilhara@gmail.com>
2022-01-17 11:13:21 +0530

    add category controller impl

---
+++ src/main/java/lk/ac/iit/finance/app/model/CategoryType.java
@@ -0,0 +1,5 @@
+package lk.ac.iit.finance.app.model;
+
+public enum CategoryType {
+    INCOME,EXPENSE
+}
--- src/main/java/lk/ac/iit/finance/app/servlet/CategoryController.java
+++ src/main/java/lk/ac/iit/finance/app/servlet/CategoryController.java
@@ -1,11 +1,15 @@
 package lk.ac.iit.finance.app.servlet;
 
+import lk.ac.iit.finance.app.manager.CategoryManager;
+import lk.ac.iit.finance.app.model.CategoryType;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/categories")
 public class CategoryController extends HttpServlet {
@@ -14,10 +18,34 @@ public class CategoryController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        CategoryManager categoryManager = CategoryManager.getInstance();
+        categoryManager.getIncomeCategoryList();
+        categoryManager.getExpenseCategoryList();
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        CategoryManager categoryManager = CategoryManager.getInstance();
+        String categoryName = req.getParameter("categoryName");
+        String categoryDescription = req.getParameter("categoryDescription");
+        String categoryType = req.getParameter("categoryType");
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            System.out.println("No login session found..!!!");
+            resp.sendRedirect("");
+            return;
+        }
+        String userId = (String) session.getAttribute("userId");
+
+        if (categoryType.equals(CategoryType.EXPENSE.toString())) {
+            categoryManager.addExpenseCategory(categoryName, categoryDescription, userId, false, null);
+            resp.sendRedirect("");
+        } else if (categoryType.equals((CategoryType.INCOME.toString()))) {
+            categoryManager.addIncomeCategory(categoryName, categoryDescription, userId, false);
+            resp.sendRedirect("");
+        } else {
+            System.out.println("Invalid category type ..!!!");
+            resp.sendRedirect("");
+        }
     }
 }
